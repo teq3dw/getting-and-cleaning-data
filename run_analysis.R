f05dbe5c238f7599e45ffe2183be05e53cc161d2
Sebastian Bayer <Sebastian.Bayer@cyren.com>
2015-07-25 23:20:26 +0200

    fixed typo in ColNames

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,3 @@
-library(dplyr)
-
 # read training data
 trainData <- read.table("./data/train/X_train.txt")
 trainLabel <- read.table("./data/train/y_train.txt")
@@ -14,7 +12,6 @@ testSubject <- read.table("./data/test/subject_test.txt")
 joinedData <- rbind(trainData, testData)
 joinedLabel <- rbind(trainLabel, testLabel)
 joinedSubject <- rbind(trainSubject, testSubject)
-
 
 # extract mean and standard deviation for each measurement
 features <- read.table("./data/features.txt")
@@ -31,8 +28,8 @@ names(filteredData) <- gsub("std", "SD", names(filteredData)) # rename std to SD
 #make descriptive labels for the joinedLabels DF
 activity <- read.table("./data/activity_labels.txt")
 activity[, 2] <- tolower(gsub("_", "", activity[, 2])) # add index to first column, remove _ and lowercase everything
-substr(activity[2, 2], 8, 8) <- toupper(substr(activity[2:3, 2], 8, 8)) # capitalize letter 8 in table position 2,2 (U in walkingUpstairs)
-#substr(activity[2, 2], 8, 8) <- toupper(substr(activity[3, 2], 8, 8)) # capitalize letter 8 in table position 3,2 (D in walkingDownstairs)
+substr(activity[2, 2], 8, 8) <- toupper(substr(activity[2, 2], 8, 8)) # capitalize letter 8 in table position 2,2 (U in walkingUpstairs)
+substr(activity[3, 2], 8, 8) <- toupper(substr(activity[3, 2], 8, 8)) # capitalize letter 8 in table position 3,2 (D in walkingDownstairs)
 activityLabels <- activity[joinedLabel[,1], 2] # match activity description from df activity to df joinedlabel by ID
 joinedLabel[, 1] <- activityLabels # join activitylabels character vector to joinedLabel df
 names(joinedLabel) <- "activity"
@@ -43,13 +40,10 @@ names(joinedSubject) <- "subject"
 cleanedData <- cbind(joinedSubject, joinedLabel, filteredData)
 
 #write the requested text file merged_data.txt containing the cleaned dataset
-write.table(cleanedData, "merged_data.txt") 
+write.table(cleanedData, "merged_data.txt", row.name=FALSE) 
 
 
 # create the second, independent tidy data set with averages of each variable for each activity and each subject.
-#nosubjects <- length(table(joinedSubject))
-#activitycount <- dim(activity)[1]
-#columns <- dim(cleanedData)[2]
 myres <- matrix(NA, nrow=length(table(joinedSubject))*dim(activity)[1], ncol=dim(cleanedData)[2]) # create a matrix with the reqiored dimensions
 result <- as.data.frame(myres) # convert to dataframe
 colnames(result) <- colnames(cleanedData)  # copy cleanedData column names to our result DF
@@ -65,7 +59,7 @@ for(i in 1:length(table(joinedSubject))){   # iterate through subjects (i)
     myrow <- myrow + 1 # iterate to next row
   }
 }
-write.table(result, "data_with_means.txt")
+write.table(result, "data_with_means.txt", row.name=FALSE)
 
 
 
